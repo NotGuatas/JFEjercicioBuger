ccea440780300e4ed384da84139e0552ab5674b0
laboratorio <laboratorio@UPEAULA-31715>
2024-10-21 10:15:11 -0500

    Agregar archivos de proyecto.

---
+++ Models/Burguer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JFEjercicioBuger.Models
+{
+    public class Burguer
+    {
+        public int BurgerId {get;set;}
+        [Required]
+
+        public string? Name { get; set; }
+
+        public bool WithCheese { get; set; }
+
+        public decimal Price { get; set; }
+
+    }
+}
