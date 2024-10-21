0f476d3a743774f85076fbb4fc18c513cdcd1909
NotGuatas <josephaugustoflores@gmail.com>
2024-10-21 10:28:07 -0500

    Creación de clase con sus atributos

--- Models/Burguer.cs
+++ Models/Burguer.cs
@@ -4,12 +4,13 @@ namespace JFEjercicioBuger.Models
 {
     public class Burguer
     {
-        public int BurgerId {get;set;}
+        public int BurgerId { get; set; }
         [Required]
 
         public string? Name { get; set; }
 
         public bool WithCheese { get; set; }
+        [Range(0.01, 99.99)]
 
         public decimal Price { get; set; }
 
